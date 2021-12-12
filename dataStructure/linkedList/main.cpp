a3208f647dbe3dc005df6e9b8dbe7aaaddebe5d5
artur_b <artur.badalyan.im@gmail.com>
2021-12-12 14:56:57 +0400

    Added linked list implementation

--- dataStructure/linkedList/main.cpp
+++ dataStructure/linkedList/main.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
-int main()
+using namespace std;
+
+class Node {
+    public: 
+        int data;
+        Node *next;
+};
+
+void append(Node** headRef, int newData) {
+    Node* newNode = new Node();
+    Node *last = *headRef;
+
+    newNode->data = newData;
+    newNode->next = NULL;
+
+    if (*headRef == NULL) {
+        *headRef = newNode;
+        return;
+    }
+
+    while (last->next != NULL) {
+        last = last->next;
+    }
+
+    last->next = newNode;
+    return;
+}
+
+void deleteNode(Node** headRef, int key)
 {
-    std::cout << "Hello Geek\n";
+    Node* temp = *headRef;
+    Node* prev = NULL;
+
+    if (temp != NULL && temp->data == key) {
+        *headRef = temp->next; 
+        delete temp;            
+        return;
+    } else {
+        while (temp != NULL && temp->data != key)
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+ 
+        if (temp == NULL)
+            return;
+    
+        prev->next = temp->next;
+    
+        delete temp;
+    }
+}
+
+void printList(Node *node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+}
+
+int main() {
+    Node* head = NULL;
+
+
+    printList(head);
+    append(&head, 1);
+    append(&head, 2);
+    append(&head, 3);
+
+    cout<<"Created Linked list is: ";
+    printList(head);
+
+
     return 0;
 }
